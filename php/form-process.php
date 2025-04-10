835cd9a771edd302ad61703d9a278acc6e54666d
abedelmabod <110660699+abedelmabod@users.noreply.github.com>
2025-04-10 16:27:14 +0200

    Add files via upload

--- php/form-process.php
+++ php/form-process.php
@@ -40,7 +40,7 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "armanmia7@gmail.com";
+$EmailTo = "biboaboshady2002@gmail.com";
 $Subject = "New Message Received";
 
 // prepare email body text
